bb14eb3004345eb1467750901263743494921e5e
valery <ebalaika1994@gmail.com>
2022-06-19 17:44:40 +0300

    17 task

---
+++ 17/17.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+//Реализовать бинарный поиск встроенными методами языка.
+
+func main() {
+	slice := []int{-4, -1, 0, 0, 5, 7, 9, 12, 15, 15, 17, 20, 22}
+	element, err := binarySearch(slice, 26)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(element)
+	}
+
+}
+
+func binarySearch(slice []int, value int) (int, error) {
+	err := errors.New("no such element")
+
+	left, right := 0, len(slice)-1
+	for left <= right {
+		mid := (left + right) / 2
+		if slice[mid] == value {
+			return mid, nil
+		}
+
+		if slice[mid] < value {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return 0, err
+}
