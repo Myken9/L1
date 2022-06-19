85e4034a58c6d57c4f4e9690788cabf2d780c279
valery <ebalaika1994@gmail.com>
2022-06-19 13:48:26 +0300

    10 task

---
+++ 10/10.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+//Дана последовательность температурных колебаний: -25.4, -27.0 13.0, 19.0, 15.5, 24.5, -21.0, 32.5.
+//Объединить данные значения в группы с шагом в 10 градусов. Последовательность в подмножноствах не важна.
+//Пример: -20:{-25.0, -27.0, -21.0}, 10:{13.0, 19.0, 15.5}, 20: {24.5}, etc.
+
+func main() {
+	row := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+	newMap := Map{m: make(map[int][]float64)}
+
+	for _, v := range row {
+		newMap.Store(int(v/10)*10, v)
+	}
+	fmt.Println(newMap)
+}
+
+type Map struct {
+	m map[int][]float64
+}
+
+func (m *Map) Load(key int) ([]float64, bool) {
+	val, ok := m.m[key]
+	return val, ok
+}
+
+func (m *Map) Store(key int, value float64) {
+	m.m[key] = append(m.m[key], value)
+}
